f02fad8c5d5437684332099abe0b6eb62599c4ea
Demis Gallisto <demis.gallisto@gmail.com>
2018-12-11 18:42:50 +0100

    build script

--- build.sh
+++ build.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
+ls -la /vto_wasm_headtracker/
+ls -la /vto_wasm_headtracker/src/
+mkdir /vto_wasm_headtracker/src/build/
 cd /vto_wasm_headtracker/src/build/
 ls -la ./
 /emsdk/emscripten/1.38.21/emcmake cmake -DCMAKE\_BUILD\_TYPE=Release -DOpenCV\_DIR=/opencv_wasm ..
