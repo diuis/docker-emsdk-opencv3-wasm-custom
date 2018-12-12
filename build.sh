7b033c45bb1ed771c071494c42ca8d9a4de2529b
Demis Gallisto <demis.gallisto@gmail.com>
2018-12-12 09:36:15 +0100

    removed mkdir

--- build.sh
+++ build.sh
@@ -2,7 +2,6 @@
 
 ls -la /vto_wasm_headtracker/
 ls -la /vto_wasm_headtracker/src/
-mkdir /vto_wasm_headtracker/src/build/
 cd /vto_wasm_headtracker/src/build/
 ls -la ./
 /emsdk/emscripten/1.38.21/emcmake cmake -DCMAKE\_BUILD\_TYPE=Release -DOpenCV\_DIR=/opencv_wasm ..
