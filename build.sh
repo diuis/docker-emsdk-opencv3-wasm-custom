d0011e70bf47c2022f5bc3924e27ecacd1ae2b21
Demis Gallisto <demis.gallisto@gmail.com>
2018-12-18 13:11:16 +0100

    added make

--- build.sh
+++ build.sh
@@ -5,3 +5,5 @@ ls -la /vto_wasm_headtracker/src/
 cd /vto_wasm_headtracker/src/build/
 ls -la ./
 /home/appuser/emsdk/emscripten/1.38.21/emcmake cmake -DCMAKE\_BUILD\_TYPE=Release -DOpenCV\_DIR=/home/appuser/opencv_wasm ..
+make
+
