89546d43902c0d0341a6da8931cfeda1692ea263
Demis Gallisto <demis.gallisto@gmail.com>
2018-12-19 12:09:47 +0100

    wip

--- build.sh
+++ build.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
-ls -la /vto_wasm_headtracker/
-ls -la /vto_wasm_headtracker/src/
-cd /vto_wasm_headtracker/src/build/
+ls -la /home/appuser/vto_wasm_headtracker/
+ls -la /home/appuser/vto_wasm_headtracker/src/
+cd /home/appuser/vto_wasm_headtracker/src/build/
 ls -la ./
 /home/appuser/emsdk/emscripten/1.38.21/emcmake cmake -DCMAKE\_BUILD\_TYPE=Release -DOpenCV\_DIR=/home/appuser/opencv_wasm ..
 make
